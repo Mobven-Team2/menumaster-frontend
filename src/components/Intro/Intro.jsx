c72b72f8accef6c976da7cda24eba3c021046d63
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-05-27 22:28:14 +0300

    giriş introduction sectionu oluşturuldu

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import "./intro.scss"
+import introresim from "../../assets/svg/introresim.svg"
 const Intro = () => {
   return (
-    <div>
+    <>
+    <section className='introPage'>
+      <div className='introPage_text'>
+      <h3 className='introPage_text-title'>Lorem ipsum dolor sit amet consectetur. Vitae <span className='introPage_text-special'>diam vulputate</span> nunc volutpat eget tempor. Consectetur sit amet </h3>
+      <p className='introPage_text-content'>Yapay zeka destekli uygulamamız, kişisel ihtiyaçlarınıza uygun yemek tarifleri ve beslenme önerileri sunarak size bu yolda rehberlik ediyor. Uygulamayı indir ve yüzlerce çeşit yemek tariflerine göz at! </p>
+      </div>
+
+      <div className='introPage_image'>
+        <img className='introPage_image-phone' src={introresim} alt="" />
+      </div>
       
-    </div>
+    </section>
+      
+    </>
   )
 }
 
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../components/Header/header'
+import Intro from '../components/Intro/Intro'
+import './mainpage.scss'
 
 const MainPage = () => {
   return (
     <div>
       <Header/>
+      <Intro/>
     </div>
   )
 }
